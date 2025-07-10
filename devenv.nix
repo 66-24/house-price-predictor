a328d1b57ed80d8912efa921e5ba39bd58afdeea
☠️ <93435269+funsimplenimble@users.noreply.github.com>
2025-07-10 18:40:12 -0400

    feat: :sparkles: Setup Jupyter notebook using devenv. Tested EDA notebook.

---
+++ devenv.nix
@@ -0,0 +1,52 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+    packages = with pkgs; [
+    (python311.withPackages
+      (ps: with ps; [ ipykernel jupyterlab pandas matplotlib seaborn]))
+    jupyter
+    gum
+
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+      for cmd in "python --version" "git --version" "jupyter kernelspec list "; do
+        output=$($cmd)
+        gum style --foreground 212 --background 57 --bold "$output"
+      done  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
