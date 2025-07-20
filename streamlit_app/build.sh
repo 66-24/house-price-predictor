7222c411d0514d2f8dcc2d9785fad32bba550bf0
☠️ <93435269+funsimplenimble@users.noreply.github.com>
2025-07-20 17:08:49 -0400

    ✨, 📝 (Dockerfile, build.sh): Add Docker image metadata and improve build process
    The Dockerfile now includes more comprehensive metadata, such as build date, author, version, and Git SHA.  The build script is enhanced to include these metadata as build arguments and to cleanup dangling images after the build.  This improves the image's maintainability and traceability.

--- streamlit_app/build.sh
+++ streamlit_app/build.sh
@@ -5,13 +5,22 @@ GIT_SHA_SHORT=$(git rev-parse --short=7 HEAD)
 VERSION=v1
 TEAM=devops
 IMAGE=${DOCKER_USERID}/house-price-predictor-ui
+BUILD_DATE=$(date -Iseconds)
+SOURCE_DATE_EPOCH=$(date +%s)
 
 docker build \
-  --build-arg SOURCE_DATE_EPOCH=$(date +%s) \
-  -t ${IMAGE}:${VERSION} \
-  -t ${IMAGE}:${GIT_SHA_SHORT} \
-  -t ${IMAGE}:${TEAM} .
+  --build-arg SOURCE_DATE_EPOCH="$SOURCE_DATE_EPOCH" \
+  --build-arg BUILD_DATE="$BUILD_DATE" \
+  --build-arg AUTHOR="$AUTHOR" \
+  --build-arg VERSION="$VERSION" \
+  --build-arg GIT_SHA_SHORT="$GIT_SHA_SHORT" \
+  --build-arg TEAM="$TEAM" \
+  -t "${IMAGE}:${VERSION}" \
+  -t "${IMAGE}:${GIT_SHA_SHORT}" \
+  -t "${IMAGE}:${TEAM}" .
 
+#Cleanup dangling images
+docker image prune -f --filter "label=org.opencontainers.image.title=House Price Predictor UI"
 
 if [[ "$1" == "--push" ]]; then
   for tag in $VERSION $GIT_SHA_SHORT $TEAM; do
